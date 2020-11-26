8581efdbb78a1db43d9ceb0f83979aa5a9283dd3
Jack Hubert <jphubert93@gmail.com>
2020-11-26 02:01:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,10 @@ require 'pry'
 
 def reverse_each_word(string)
   word_array = string.split
+<<<<<<< HEAD
   word_array.collect {|word| word.reverse}.join(" ")
+=======
+  reverse_array = word_array.map {|word| word.reverse}.join(" ")
+  binding.pry
+>>>>>>> 0d41f252628a69fd80da819605bd29bb4fe044fb
 end
